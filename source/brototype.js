8a74dd2caffc0418186b0d22f3214d68d278ef68
David Duquenoy <David.Duquenoy@comverse.com>
2016-06-14 14:55:02 +0200

    Brototype dependency removed, as a conflict was present when angular was loaded.

--- source/brototype.js
+++ source/brototype.js
@@ -1,24 +1,169 @@
-(function(module) {
+/*global module:true, window:true, define:false*/
+(function() {
+	'use strict';
 
-	"use strict";
+	// Bromise... it's stronger than a Promise
+	function Bromise(object, method, args) {
+		this.object = object;
+		this.method = method;
+		this.args = args.length > 1 ? args.slice(1) : [];
+	}
 
-	var Bro = require("brototype");
-
-	var __iCanHaz = Bro.prototype.iCanHaz;
-
-	Bro.prototype.iCanHaz1 = function() {
-		var keys = Array.prototype.slice.call(arguments),
-			val,
-			keysLen = keys.length;
-		for (var i = 0; i < keysLen; i += 1) {
-			val = __iCanHaz.call(this, keys[i]);
-			if (val !== undefined) {
-				return val;
+	Bromise.brototype = Bromise.prototype = {
+		"butWhenIdo": function(callback, context) {
+			if (this.method instanceof Function) {
+				var returnValue = this.method.apply(this.object, this.args);
+				if (returnValue) {
+					(callback || function(){}).call(context || this.object, returnValue);
+				}
 			}
+			return context;
+		},
+
+		"hereComeTheErrors": function(callback) {
+			if (this.method instanceof Function) {
+				try {
+					this.method.apply(this.object, this.args);
+				} catch(e) {
+					callback(e);
+				}
+			} else {
+				callback(this.method + ' is not a function.');
+			}
+		},
+		"errorsAreComing": function () {
+			this.hereComeTheErrors.apply(this, arguments);
 		}
-		return undefined;
 	};
 
-	module.exports = Bro;
+	function Bro(obj) {
+		if (this instanceof Bro) {
+			this.obj = obj;
+		} else {
+			return new Bro(obj);
+		}
+	}
 
-})(module);
+	Bro.TOTALLY = true;
+	Bro.NOWAY = false;
+
+	Bro.brototype = Bro.prototype = {
+		"isThatEvenAThing": function() {
+			return this.obj !== void 0;
+		},
+
+		"doYouEven": function(key, options) {
+			var optionsBro = Bro(options || {}),
+					bro = this.iCanHaz(key);
+			if (Bro(bro).isThatEvenAThing() === Bro.TOTALLY) {
+				optionsBro.iDontAlways('forSure').butWhenIdo();
+				return Bro.TOTALLY;
+			} else {
+				optionsBro.iDontAlways('sorryBro').butWhenIdo();
+				return Bro.NOWAY;
+			}
+		},
+
+		"iCanHaz": function(key) {
+			if (Array.isArray(key)) {
+				var index, value, result = [];
+				for (index in key) {
+					value = this.iCanHaz(key[index]);
+					result.push(value);
+				}
+				return result;
+			}
+			var props = key.split('.'),
+					item = this.obj;
+			if (typeof item !== "undefined") {
+				for (var i = 0; i < props.length; i++) {
+					item = item[props[i]];
+					if (Bro(item).isThatEvenAThing() === Bro.NOWAY) {
+						return item;
+					}
+				}
+			}
+			return item;
+		},
+
+		"comeAtMe": function(brobject) {
+			var i, prop,
+					bro = Bro(brobject),
+					keys = bro.giveMeProps(),
+					obj = (this instanceof Bro) ? this.obj : Bro.prototype;
+			for (i = 0; i < keys.length; i++) {
+				prop = keys[i];
+				if (bro.hasRespect(prop)) {
+					obj[prop] = brobject[prop];
+				}
+			}
+		},
+
+		"giveMeProps": function() {
+			var key, props = [];
+			if (Object.keys) {
+				props = Object.keys(this.obj);
+			} else {
+				for (key in this.obj) {
+					if (this.obj.hasRespect(key)) {
+						props.push(key);
+					}
+				}
+			}
+			return props;
+		},
+
+		"hasRespect": function(prop) {
+			return this.obj.hasOwnProperty(prop);
+		},
+
+
+		"iDontAlways": function(methodString) {
+			var method = this.iCanHaz(methodString);
+			return new Bromise(this.obj, method, arguments);
+		},
+
+		"braceYourself": function(methodString) {
+			var method = this.iCanHaz(methodString);
+			return new Bromise(this.obj, method, arguments);
+		},
+		"makeItHappen": function(key, value) {
+			var brobj = this.obj;
+			var props = key.split('.');
+			for (var i = 0; i < props.length - 1; ++i) {
+				if (brobj[props[i]] === undefined) {
+					brobj[props[i]] = {};
+				}
+				brobj = brobj[props[i]];
+			}
+			// the deepest key is set to either an empty object or the value provided
+			brobj[props[props.length - 1]] = value === undefined ? {} : value;
+		},
+		iCanHaz1: function() { // added
+			var keys = Array.prototype.slice.call(arguments),
+					val,
+					keysLen = keys.length;
+			for (var i = 0; i < keysLen; i += 1) {
+				val = this.iCanHaz.call(this, keys[i]);
+				if (val !== undefined) {
+					return val;
+				}
+			}
+			return undefined;
+		}
+	};
+
+	(function() {
+		if (typeof define === 'function' && typeof define.amd === 'object') {
+			define(function() {
+				return Bro;
+			});
+			//} else if (typeof(angular) !== 'undefined') {
+			//angular.module('brototype', []).factory('Bro', function() { return Bro; });
+		} else if (typeof module !== 'undefined' && module.exports) {
+			module.exports = Bro;
+		} else if (typeof window !== 'undefined') {
+			window.Bro = Bro;
+		}
+	})();
+})();
