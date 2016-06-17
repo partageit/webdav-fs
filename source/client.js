e96381ffb8903a6e2aab7645dc8fd345f333ba62
David Duquenoy <David.Duquenoy@comverse.com>
2016-06-17 12:10:44 +0200

    Better way to extract base path from filename

--- source/client.js
+++ source/client.js
@@ -1,15 +1,16 @@
+/*global Buffer:true, Promise:true, require:false, module: false */
 (function(module) {
 
 	"use strict";
 
 	var fetch = require("node-fetch"),
-		xml2js = require("xml2js"),
-		querystring = require("querystring"),
-		pathTools = require("path"),
-		Bro = require("./brototype.js"),
-		Streamifier = require("streamifier");
+			xml2js = require("xml2js"),
+			querystring = require("querystring"),
+			pathTools = require("path"),
+			Bro = require("./brototype.js"),
+			url = require('url');
 
-    var STATUS_CODES = require("http").STATUS_CODES;
+	var STATUS_CODES = require("http").STATUS_CODES;
 
 	/**
 	 * Fetch raw data from a node-fetch response
@@ -22,82 +23,25 @@
 		return fetchResponse._decode().then(function() { return fetchResponse._raw; });
 	}
 
-    function getHTTPModule(protocol) {
-        return (protocol === "https") ? require("https") : require("http");
-    }
-
-    function handleResponseError(res) {
-        if (res.status >= 400) {
-            var err = new Error("Bad response: " + res.status);
-            err.httpStatusCode = res.status;
-            throw err;
-        }
-        return res;
-    }
-
-	function processDirectoryResult(path, dirResult, targetOnly) {
-		var items = [],
-			responseItems = [],
-			dirResultBro = Bro(dirResult);
-		if (targetOnly === undefined) {
-			targetOnly = false;
-		}
-		try {
-			var multistatus = dirResultBro.iCanHaz1("d:multistatus", "D:multistatus");
-			responseItems = Bro(multistatus).iCanHaz1("d:response", "D:response") || [];
-		} catch (e) {}
-		responseItems.forEach(function(responseItem) {
-			var responseBro = Bro(responseItem),
-				propstatBro = Bro(responseBro.iCanHaz1("d:propstat.0", "D:propstat.0")),
-				props = propstatBro.iCanHaz1("d:prop.0", "D:prop.0"),
-				propsBro = Bro(props);
-			//console.log(JSON.stringify(props, undefined, 4));
-			var filename = processDirectoryResultFilename(
-					path,
-					processXMLStringValue(responseBro.iCanHaz1("d:href", "D:href"))
-				).trim(),
-				resourceType = processXMLStringValue(propsBro.iCanHaz1("d:resourcetype", "D:resourcetype")),
-				itemType = (resourceType.indexOf("d:collection") >= 0 || resourceType.indexOf("D:collection") >= 0) ?
-					"directory" : "file";
-			if (filename.length <= 0) {
-				return;
-			}
-			if ((targetOnly && filename !== path) || (!targetOnly && filename === path)) {
-				// skip self or only self
-				return;
-			}
-			filename = querystring.unescape("/" + filename);
-			var item = {
-					filename: filename,
-					basename: pathTools.basename(filename),
-					lastmod: processXMLStringValue(propsBro.iCanHaz1("d:getlastmodified", "D:getlastmodified")),
-					size: parseInt(processXMLStringValue(propsBro.iCanHaz1("d:getcontentlength", "D:getcontentlength")) || "0", 10),
-					checksum: processXMLStringValue(propsBro.iCanHaz1("d:checksum", "D:checksum")),
-					type: itemType
-				},
-				mime = propsBro.iCanHaz1("d:getcontenttype", "D:getcontenttype");
-			if (mime) {
-				item.mime = mime;
-			}
-			items.push(item);
-		})
-		return items;
+	function getHTTPModule(protocol) {
+		return (protocol === "https") ? require("https") : require("http");
 	}
 
-	function processDirectoryResultFilename(path, resultFilename) {
-		var resultFLen = resultFilename.length;
-		if (resultFilename[resultFLen - 1] === "/") {
-			resultFilename = resultFilename.substr(0, resultFLen - 1);
+	function handleResponseError(res) {
+		if (res.status >= 400) {
+			var err = new Error("Bad response: " + res.status);
+			err.httpStatusCode = res.status;
+			throw err;
 		}
-		if (path === "/" || path === "") {
-			var resultParts = resultFilename.split("/");
-			return resultParts[resultParts.length - 1];
+		return res;
+	}
+
+	function getUriPathName(uri) {
+		var parsed = url.parse(uri);
+		if (parsed) {
+			return parsed.pathname;
 		}
-		var pos = resultFilename.indexOf(path);
-		if (pos >= 0) {
-			return resultFilename.substr(pos);
-		}
-		return "";
+		return '';
 	}
 
 	function processXMLStringValue(xmlVal) {
@@ -113,6 +57,74 @@
 		return "";
 	}
 
+	function processDirectoryResultFilename(path, resultFilename, basePath) {
+		//console.log('parameters in processDirectoryResultFilename: ', path, resultFilename, basePath);
+		var resultFLen = resultFilename.length;
+		if (resultFilename[resultFLen - 1] === "/") {
+			resultFilename = resultFilename.substr(0, resultFLen - 1);
+		}
+		if (path === "/" || path === "") {
+			var resultParts = resultFilename.split("/");
+			return resultParts[resultParts.length - 1];
+		}
+		var pos = resultFilename.indexOf(basePath);
+		if (pos === 0) {
+			//console.log('processDirectoryResultFilename result: ', resultFilename.substr(basePath.length));
+			return resultFilename.substr(basePath.length);
+		}
+		return "";
+	}
+
+	function processDirectoryResult(path, dirResult, basePath, targetOnly) {
+		var items = [],
+				responseItems = [],
+				dirResultBro = Bro(dirResult);
+		if (targetOnly === undefined) {
+			targetOnly = false;
+		}
+		try {
+			var multistatus = dirResultBro.iCanHaz1("d:multistatus", "D:multistatus");
+			responseItems = Bro(multistatus).iCanHaz1("d:response", "D:response") || [];
+		} catch (e) {}
+		responseItems.forEach(function(responseItem) {
+			var responseBro = Bro(responseItem),
+					propstatBro = Bro(responseBro.iCanHaz1("d:propstat.0", "D:propstat.0")),
+					props = propstatBro.iCanHaz1("d:prop.0", "D:prop.0"),
+					propsBro = Bro(props);
+			//console.log(JSON.stringify(props, undefined, 4));
+			var filename = processDirectoryResultFilename(
+				path,
+				processXMLStringValue(responseBro.iCanHaz1("d:href", "D:href")),
+				basePath
+			).trim(),
+					resourceType = processXMLStringValue(propsBro.iCanHaz1("d:resourcetype", "D:resourcetype")),
+					itemType = (resourceType.indexOf("d:collection") >= 0 || resourceType.indexOf("D:collection") >= 0) ?
+					"directory" : "file";
+			if (filename.length <= 0) {
+				return;
+			}
+			if ((targetOnly && filename !== path) || (!targetOnly && filename === path)) {
+				// skip self or only self
+				return;
+			}
+			filename = querystring.unescape("/" + filename);
+			var item = {
+				filename: filename,
+				basename: pathTools.basename(filename),
+				lastmod: processXMLStringValue(propsBro.iCanHaz1("d:getlastmodified", "D:getlastmodified")),
+				size: parseInt(processXMLStringValue(propsBro.iCanHaz1("d:getcontentlength", "D:getcontentlength")) || "0", 10),
+				checksum: processXMLStringValue(propsBro.iCanHaz1("d:checksum", "D:checksum")),
+				type: itemType
+			},
+					mime = propsBro.iCanHaz1("d:getcontenttype", "D:getcontenttype");
+			if (mime) {
+				item.mime = mime;
+			}
+			items.push(item);
+		});
+		return items;
+	}
+
 	function removePathPrefixSlash(path) {
 		if (path[0] === "/") {
 			path = path.substr(1);
@@ -120,8 +132,8 @@
 		return path.trim();
 	}
 
-    function enforcePathPrefixSlash(path) {
-        path = path.trim();
+	function enforcePathPrefixSlash(path) {
+		path = path.trim();
 		if (path[0] !== "/") {
 			path ="/" + path;
 		}
@@ -136,8 +148,8 @@
 				throw new Error("Cannot delete root");
 			}
 			return fetch(auth.url + path, {
-					method: "DELETE"
-				})
+				method: "DELETE"
+			})
 				.then(handleResponseError);
 		},
 
@@ -147,40 +159,40 @@
 				path = "/";
 			}
 			return fetch(auth.url + path, {
-					method: "PROPFIND",
-					headers: {
-						Depth: 1
-					}
-				})
+				method: "PROPFIND",
+				headers: {
+					Depth: 1
+				}
+			})
 				.then(handleResponseError)
-                .then(function(res) {
-                    return res.text();
-                })
+				.then(function(res) {
+				return res.text();
+			})
 				.then(function(body) {
-					var parser = new xml2js.Parser();
-					return new Promise(function(resolve, reject) {
-						parser.parseString(body, function (err, result) {
-							if (err) {
-								(reject)(err);
-							} else {
-								(resolve)(processDirectoryResult(path, result));
-							}
-						});
+				var parser = new xml2js.Parser();
+				return new Promise(function(resolve, reject) {
+					parser.parseString(body, function (err, result) {
+						if (err) {
+							(reject)(err);
+						} else {
+							(resolve)(processDirectoryResult(path, result, getUriPathName(auth.url)));
+						}
 					});
 				});
+			});
 		},
 
 		getFile: function(auth, path, encoding) {
 			encoding = (encoding || "utf8").toLowerCase();
 			path = removePathPrefixSlash(path);
 			return fetch(auth.url + path)
-                .then(handleResponseError)
+				.then(handleResponseError)
 				.then(function(res) {
-					if (encoding === "utf8") {
-						return res.text();
-					}
-					return fetchRaw(res);
-				});
+				if (encoding === "utf8") {
+					return res.text();
+				}
+				return fetchRaw(res);
+			});
 		},
 
 		getStat: function(auth, path) {
@@ -189,40 +201,40 @@
 				path = "/";
 			}
 			return fetch(auth.url + path, {
-					method: "PROPFIND",
-					headers: {
-						Depth: 1
-					}
-				})
-                .then(handleResponseError)
+				method: "PROPFIND",
+				headers: {
+					Depth: 1
+				}
+			})
+				.then(handleResponseError)
 				.then(function(res) {
-					return res.text();
-				})
+				return res.text();
+			})
 				.then(function(body) {
-					var parser = new xml2js.Parser();
-					return new Promise(function(resolve, reject) {
-						parser.parseString(body, function (err, result) {
-							if (err) {
-								(reject)(err);
-							} else {
-								(resolve)(processDirectoryResult(path, result, true));
-							}
-						});
+				var parser = new xml2js.Parser();
+				return new Promise(function(resolve, reject) {
+					parser.parseString(body, function (err, result) {
+						if (err) {
+							(reject)(err);
+						} else {
+							(resolve)(processDirectoryResult(path, result, getUriPathName(auth.url), true));
+						}
 					});
 				});
+			});
 		},
 
-        moveFile: function(auth, remoteFilePath, targetFilePath) {
-            remoteFilePath = removePathPrefixSlash(remoteFilePath);
-            targetFilePath = removePathPrefixSlash(targetFilePath);
-            return fetch(auth.url + remoteFilePath, {
-                    method: "MOVE",
-                    headers: {
-                        Destination: auth.url + targetFilePath
-                    }
-                })
-                .then(handleResponseError);
-        },
+		moveFile: function(auth, remoteFilePath, targetFilePath) {
+			remoteFilePath = removePathPrefixSlash(remoteFilePath);
+			targetFilePath = removePathPrefixSlash(targetFilePath);
+			return fetch(auth.url + remoteFilePath, {
+				method: "MOVE",
+				headers: {
+					Destination: auth.url + targetFilePath
+				}
+			})
+				.then(handleResponseError);
+		},
 
 		putFile: function(auth, path, data, encoding) {
 			encoding = (encoding || "utf8").toLowerCase();
@@ -232,53 +244,53 @@
 			} else if (encoding === "binary") {
 				mime = "application/octet-stream";
 				if (typeof data === "string") {
-                    data = new Buffer(data, "binary");
+					data = new Buffer(data, "binary");
 				}
 			} else {
 				throw new Error("Unknown or unspecified encoding");
 			}
 			path = enforcePathPrefixSlash(path);
-            var http = getHTTPModule(auth.protocol),
-                headers = {
-                    "Content-Type": mime,
-                    "Content-Length": data.length
-                };
-            if (auth.username.length > 0) {
-                headers.Authorization = "Basic " +
-                    (new Buffer(auth.username + ":" + auth.password)).toString('base64');
-            }
-            return new Promise(function(resolve, reject) {
-                var request = http.request(
-                    {
-                        host: auth.host,
-                        port: auth.port,
-                        path: path,
-                        method: "PUT",
-                        headers: headers
-                    },
-                    function(response) {
-                        var statusCode = response.statusCode;
-                        if (statusCode >= 400) {
-                            var err = new Error("Server returned error: " + statusCode + " " +
-                                STATUS_CODES[statusCode]);
-                            err.httpStatusCode = statusCode;
-                            (reject)(err);
-                        } else {
-                            (resolve)();
-                        }
-                    }
-                );
-                request.on("error", reject);
-                request.write(data);
-                request.end();
-            });
+			var http = getHTTPModule(auth.protocol),
+					headers = {
+						"Content-Type": mime,
+						"Content-Length": data.length
+					};
+			if (auth.username.length > 0) {
+				headers.Authorization = "Basic " +
+					(new Buffer(auth.username + ":" + auth.password)).toString('base64');
+			}
+			return new Promise(function(resolve, reject) {
+				var request = http.request(
+					{
+						host: auth.host,
+						port: auth.port,
+						path: path,
+						method: "PUT",
+						headers: headers
+					},
+					function(response) {
+						var statusCode = response.statusCode;
+						if (statusCode >= 400) {
+							var err = new Error("Server returned error: " + statusCode + " " +
+																	STATUS_CODES[statusCode]);
+							err.httpStatusCode = statusCode;
+							(reject)(err);
+						} else {
+							(resolve)();
+						}
+					}
+				);
+				request.on("error", reject);
+				request.write(data);
+				request.end();
+			});
 		},
 
 		putDir: function(auth, path) {
 			path = removePathPrefixSlash(path);
 			return fetch(auth.url + path, {
-					method: "MKCOL"
-				})
+				method: "MKCOL"
+			})
 				.then(handleResponseError);
 		}
 
